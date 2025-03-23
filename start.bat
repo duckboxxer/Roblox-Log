863379ce42e3de30e0a1351211d7270e0b93eb04
少女。 <beehive_dev@outlook.jp>
2025-03-23 10:57:04 +0900

    Update
    Support - 
    Fishstrap
    Roblox
    Bloxstrap

--- start.bat
+++ start.bat
@@ -1,4 +1,7 @@
 @echo off
+color 4
 del /q /f "%localappdata%\Roblox\logs\*"
+del /q /f "%localappdata%\Fishstrap\logs\*"
+del /q /f "%localappdata%\Bloxstrap\logs\*"
 echo fucki byfron nigga
 pause
